f890fc6828634049d5edb1081eef2049eeac82ce
Ben Lin <benlin131020@gmail.com>
2019-12-13 11:54:02 +0800

    Updated timer and notification

--- MyApplication/app/src/main/java/com/example/myapplication/Timer.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Timer.java
@@ -16,10 +16,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.os.CountDownTimer;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Timer extends AppCompatActivity {
 
-    String CHANNEL_ID = "my_channel_01";
     private NotificationManagerCompat notificationManager;
+    public int set, worktime, idletime, cnt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +37,10 @@ public class Timer extends AppCompatActivity {
     }
 
 
-    public void notification(){
-
+    public void notification(String text){
+        String CHANNEL_ID = "my_channel_01";
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            String CHANNEL_ID = "my_channel_01";
+            CHANNEL_ID = "my_channel_01";
             CharSequence name = "my_channel";
             String Description = "This is my channel";
             int importance = NotificationManager.IMPORTANCE_HIGH;
@@ -52,31 +57,72 @@ public class Timer extends AppCompatActivity {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle("Timer")
-                .setContentText("Done!")
+                .setContentText(text)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         notificationManager.notify(1, builder.build());
     }
 
-    private View.OnClickListener start_timer = new View.OnClickListener(){
+    public void timer(){
+        if(cnt == set * 2){
+            return;
+        }else if(cnt % 2 == 0){
+            new CountDownTimer(worktime, 1000) {
 
-        @Override
-        public void onClick(View v) {
-
-            new CountDownTimer(3000, 1000) {
-
-                TextView fieldTime = (TextView)findViewById(R.id.textViewTime) ;
+                TextView fieldTime = (TextView)findViewById(R.id.textViewRemainTime) ;
 
                 public void onTick(long millisUntilFinished) {
                     fieldTime.setText("seconds remaining: " + millisUntilFinished / 1000);
                 }
 
                 public void onFinish() {
-                    notification();
-                    fieldTime.setText("done!");
-                    // notificationId is a unique int for each notification that you must define
+                    notification("Set " + Integer.toString(cnt / 2 + 1) + " Done!");
+                    fieldTime.setText("set done!");
+                    cnt += 1;
+                    timer();
                 }
             }.start();
+        }else{
+            new CountDownTimer(idletime, 1000) {
+
+                TextView fieldTime = (TextView)findViewById(R.id.textViewRemainTime) ;
+
+                public void onTick(long millisUntilFinished) {
+                    fieldTime.setText("seconds remaining: " + millisUntilFinished / 1000);
+                }
+
+                public void onFinish() {
+                    notification("Idle " + Integer.toString(cnt / 2 + 1) + " Done!");
+                    fieldTime.setText("idle done!");
+                    cnt += 1;
+                    timer();
+                }
+            }.start();
+        }
+
+    }
+
+    private View.OnClickListener start_timer = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            EditText fieldset = (EditText)findViewById(R.id.editTextSet) ;
+            set = Integer.parseInt(fieldset.getText().toString());
+
+            EditText fieldworktime = (EditText)findViewById(R.id.editTextWorkTime) ;
+            String[] timeStr = fieldworktime.getText().toString().split(":");
+            int min = Integer.parseInt(timeStr[0]);
+            int sec = Integer.parseInt(timeStr[1]);
+            worktime = (min * 60 + sec) * 1000;
+
+            EditText fieldidletime = (EditText)findViewById(R.id.editTextIdleTime) ;
+            timeStr = fieldidletime.getText().toString().split(":");
+            min = Integer.parseInt(timeStr[0]);
+            sec = Integer.parseInt(timeStr[1]);
+            idletime = (min * 60 + sec) * 1000;
+
+            cnt = 0;
+            timer();
 
         }
 
